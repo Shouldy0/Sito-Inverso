cad4c3da674cf43aafc11da3b0246d58582081b9
Daiana <shouldy01@gmail.com>
2026-06-20 13:53:42 +0200

    Install @vercel/analytics and integrate into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import InfoPage from './pages/InfoPage';
 import Checkout from './pages/Checkout';
 import Admin from './pages/Admin';
 import { CartProvider } from './context/CartContext';
+import { Analytics } from '@vercel/analytics/react';
 import './index.css';
 
 function App() {
@@ -32,6 +33,7 @@ function App() {
           </Route>
         </Routes>
       </Router>
+      <Analytics />
     </CartProvider>
   );
 }
